e4024dcb5467152e90ff851addf1dc576db007e2
abeecool <abeecool1480@gmail.com>
2025-09-02 18:39:54 +0530

    First Commit
    First Date: 09-02-2025

---
+++ app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+
+app.listen(4000, ()=> {
+  console.log("Server Started Abee");
+});
+
+app.post("/post", async(req, res)=> {
+  console.log(req.body);
+
+  const {data} = req.body;
+  if(data == "cool"){
+    res.send({status:'Abee Cool'});
+  }
+});
